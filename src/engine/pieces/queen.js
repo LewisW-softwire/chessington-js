c13980bc16510f42e9d42d739203a03f84ba9d12
Lewis Wakeland <Lewis.Wakeland@softwire.com>
2018-07-02 15:15:18 +0100

    Passed test Red-11

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -7,32 +7,7 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        let currentLocation = board.findPiece(this);
-        let availableMoves = [];
-        let diff = currentLocation.row - currentLocation.col;
-        let sum = currentLocation.row + currentLocation.col;
-        //Diagonal moves
-        for (let i = 0; i <= sum; i++) {
-            if (i != currentLocation.row) {
-                availableMoves.push(Square.at(i, sum - i));
-            }
-        }
-        for (let i = 0; i <= 7; i++) {
-            if (i - diff >= 0 && i - diff <= 7 && i != currentLocation.row) {
-                availableMoves.push(Square.at(i, i - diff));
-            }
-        }
-        //Vertical and horizontal moves
-        
-        for(let i = 0; i<=7; i++){
-            if(i !== currentLocation.col){
-                availableMoves.push(Square.at(currentLocation.row, i));
-            }
-            if(i !== currentLocation.row){
-                availableMoves.push(Square.at(i, currentLocation.col));
-            }
-        }
-
-        return availableMoves;
+        let possibleDirections = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]];
+        return Piece.getLineMoves(board, possibleDirections, board.findPiece(this));
     }
 }
