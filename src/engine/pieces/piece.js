b93308958d62bc7613f5a1167ccf15f9dd3a4c96
Lewis Wakeland <Lewis.Wakeland@softwire.com>
2018-07-02 15:13:57 +0100

    Passed test Red-10

--- src/engine/pieces/piece.js
+++ src/engine/pieces/piece.js
@@ -1,3 +1,6 @@
+import Square from '../square';
+import Board from '../board';
+
 export default class Piece {
     constructor(player) {
         this.player = player;
@@ -12,5 +15,17 @@ export default class Piece {
         const currentSquare = board.findPiece(this);
         board.movePiece(currentSquare, newSquare);
         this.firstmove = false;
+    }
+
+    static getLineMoves(board, directions, currentLocation) {
+        let availableMoves = [];
+        for (let i = 0; i < directions.length; i++) {
+            for (let j = 1; j <= 7; j++) {
+                let position = Square.at(currentLocation.row + directions[i][0] * j, currentLocation.col + directions[i][1] * j);
+                if (!Board.validateSquare(position) || board.getPiece(position)) break;
+                availableMoves.push(position);
+            }
+        }
+        return availableMoves;
     }
 }
--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,6 +1,4 @@
 import Piece from './piece';
-import Square from '../square';
-import Board from '../board';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -8,18 +6,7 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        let currentLocation = board.findPiece(this);
-        let availableMoves = [];
-        let possibleDirections = [[1,0],[0,1],[-1,0],[0,-1]];
-        for (let i=0;i<possibleDirections.length;i++)
-        {
-            for (let j=1;j<=7;j++)
-            {
-                let position = Square.at(currentLocation.row + possibleDirections[i][0]*j, currentLocation.col + possibleDirections[i][1] * j);
-                if (! Board.validateSquare(position) || board.getPiece(position)) break;
-                availableMoves.push(position);
-            }
-        }
-        return availableMoves;
+        let possibleDirections = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+        return Piece.getLineMoves(board, possibleDirections, board.findPiece(this));
     }
 }
