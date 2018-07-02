a01fbba5532ac9230ab82099b86a79a2d10113db
Lewis Wakeland <Lewis.Wakeland@softwire.com>
2018-07-02 15:45:38 +0100

    Passed test Red-18

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -8,6 +8,11 @@ export default class Pawn extends Piece {
         super(player);
     }
 
+    pawnTakingPiece(board, position)
+    {
+        return Board.validateSquare(position) && board.getPiece(position) && (board.getPiece(position).player !== this.player) && (board.getPiece(position).constructor.name != "King");
+    }
+
     getAvailableMoves(board) {
         let currentLocation = board.findPiece(this);
         let availableMoves = [];
@@ -15,9 +20,21 @@ export default class Pawn extends Piece {
         let direction = this.player === Player.WHITE ? 1 : -1;
         let squareAhead = Square.at(currentLocation.row + direction, currentLocation.col);
         let squareTwoAhead = Square.at(currentLocation.row + (direction * 2), currentLocation.col);
+
+        //move
         if (Board.validateSquare(squareAhead) && !board.getPiece(squareAhead)) {
             availableMoves = [squareAhead];
             if (Board.validateSquare(squareTwoAhead) && this.firstmove && !board.getPiece(squareTwoAhead)) availableMoves.push(squareTwoAhead);
+        }
+
+        //taking pieces
+        let squareAheadLeft = Square.at(currentLocation.row + direction, currentLocation.col-1);
+        let squareAheadRight = Square.at(currentLocation.row + direction, currentLocation.col+1);
+        if (this.pawnTakingPiece(board,squareAheadLeft)) {
+            availableMoves.push(squareAheadLeft);
+        }
+        if (this.pawnTakingPiece(board,squareAheadRight)) {
+            availableMoves.push(squareAheadRight);
         }
         return availableMoves;
     }
