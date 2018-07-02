585df5e5a410e4dbeddb75a5dcad0037748e7fc0
Lewis Wakeland <Lewis.Wakeland@softwire.com>
2018-07-02 14:32:05 +0100

    Passed test Red-4

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,21 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentLocation = board.findPiece(this);
+        let availableMoves = [];
+        let diff = currentLocation.row - currentLocation.col;
+        let sum = currentLocation.row + currentLocation.col;
+        for (let i = 0; i <= sum; i++) {
+            if (i != currentLocation.row) {
+                availableMoves.push(Square.at(i, sum - i));
+            }
+        }
+        for (let i = 0; i <= 7; i++) {
+            if (i - diff >= 0 && i - diff <= 7 && i != currentLocation.row) {
+                availableMoves.push(Square.at(i, i - diff));
+            }
+        }
+
+        return availableMoves;
     }
 }
