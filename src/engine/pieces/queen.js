32bf7f91deb589c641bb6342fabe491532dee7fc
Lewis Wakeland <Lewis.Wakeland@softwire.com>
2018-07-02 14:34:03 +0100

    Passed test Red-5

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +7,32 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentLocation = board.findPiece(this);
+        let availableMoves = [];
+        let diff = currentLocation.row - currentLocation.col;
+        let sum = currentLocation.row + currentLocation.col;
+        //Diagonal moves
+        for (let i = 0; i <= sum; i++) {
+            if (i != currentLocation.row) {
+                availableMoves.push(Square.at(i, sum - i));
+            }
+        }
+        for (let i = 0; i <= 7; i++) {
+            if (i - diff >= 0 && i - diff <= 7 && i != currentLocation.row) {
+                availableMoves.push(Square.at(i, i - diff));
+            }
+        }
+        //Vertical and horizontal moves
+        
+        for(let i = 0; i<=7; i++){
+            if(i !== currentLocation.col){
+                availableMoves.push(Square.at(currentLocation.row, i));
+            }
+            if(i !== currentLocation.row){
+                availableMoves.push(Square.at(i, currentLocation.col));
+            }
+        }
+
+        return availableMoves;
     }
 }
