e918091756a5a6a5dbbd8b477317d709cfc7a662
Lewis Wakeland <Lewis.Wakeland@softwire.com>
2018-07-02 14:08:45 +0100

    Passed test Red-1

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+import Player from '../player';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,11 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentLocation = board.findPiece(this);
+        if(this.player === Player.WHITE){
+            return [Square.at(currentLocation.row+1, currentLocation.col)];
+        }else{
+            return [Square.at(currentLocation.row-1, currentLocation.col)];
+        }
     }
 }
